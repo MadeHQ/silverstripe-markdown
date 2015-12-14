f425db95aff35ae6ef426f8bd1b233e011ba39b5
Nivanka Fonseka <nivanka@silverstripers.com>
2015-12-14 14:53:18 +0530

    updated the inline JS include

--- _config.php
+++ _config.php
@@ -5,8 +5,6 @@ define('MARKDOWN_MODULE_BASE', basename(dirname(__FILE__)));
 
 MarkdownExtension::ReplaceHTMLFields();
 
-MarkdownEditorField::include_default_js();
-
 if(ClassInfo::exists('CloudinaryUploadField')){
     MarkdownEditorField::add_extension('MarkdownCloudinaryUpload');
 }
--- code/extensions/MarkdownLeftAndMainExtension.php
+++ code/extensions/MarkdownLeftAndMainExtension.php
@@ -2,6 +2,10 @@
 
 class MarkdownLeftAndMainExtension extends Extension {
 
+	public function onAfterInit(){
+		MarkdownEditorField::include_default_js();
+	}
+
     public function TextileEditorToolbar() {
         return MarkdownEditorField_Toolbar::create($this, "EditorToolbar");
     }
