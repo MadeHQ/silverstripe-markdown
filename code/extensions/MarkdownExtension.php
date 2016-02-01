52d235bfa8867905291933f8ef8ea9f14d794c03
priyashantha <priyashanthahp@gmail.com>
2016-02-01 15:28:18 +0530

    removed config manifest caching as we've got issues with that

--- code/extensions/MarkdownExtension.php
+++ code/extensions/MarkdownExtension.php
@@ -9,72 +9,49 @@
 
 class MarkdownExtension extends DataExtension {
 
-	public static function ReplaceHTMLFields(){
+    private static $replace_html_fields = true;
 
-		if(isset($_REQUEST['flush']) && $_REQUEST['flush'] == 'all'){
-			$classes = ClassInfo::subclassesFor('DataObject');
-			$configManifest = new SS_ConfigStaticManifest(BASE_PATH, false, true);
-			$static = $configManifest->getStatics();
+    public static function ReplaceHTMLFields(){
+        if(Config::inst()->get('MarkdownExtension', 'replace_html_fields')){
+            $classes = ClassInfo::subclassesFor('DataObject');
+            foreach($classes as $className){
+                if($db = Config::inst()->get($className, 'db')){
+                    if(in_array('HTMLText', $db)){
+                        $updateDB = array();
+                        foreach($db as $field => $type){
+                            $newType = $type;
+                            if(strpos($type, 'HTMLText') !== false){
+                                $newType = str_replace($type, 'HTMLText', 'MarkdownText');
+                            }
+                            if(strpos($type, 'HTMLVarchar') !== false){
+                                $newType = str_replace($type, 'HTMLVarchar', 'MarkdownVarchar');
+                            }
 
-			$arrUpdated = array();
+                            $updateDB[$field] = $newType;
+                        }
 
-			foreach($static as $className => $options){
-				if(in_array($className, $classes) && isset($options['db']) && isset($options['db']['value'])){
-					$bUpdated = false;
-					foreach($options['db']['value'] as $field => $type){
-						$newType = $type;
-						if(strpos($type, 'HTMLText') !== false){
-							$newType = str_replace($type, 'HTMLText', 'MarkdownText');
-							$bUpdated = true;
-						}
-						if(strpos($type, 'HTMLVarchar') !== false){
-							$newType = str_replace($type, 'HTMLVarchar', 'MarkdownVarchar');
-							$bUpdated = true;
-						}
-						$options['db']['value'][$field] = $newType;
-					}
-
-					if($bUpdated == true){
-						$arrUpdated[$className] = $options;
-						Config::inst()->update($className, 'db', $options['db']['value']);
-					}
-
-				}
-			}
-
-			if(count($arrUpdated)){
-				$cacheClass = defined('SS_MANIFESTCACHE') ? SS_MANIFESTCACHE : 'ManifestCache_File';
-				$cache = new $cacheClass('staticmanifest');
-				$pathKey = sha1(BASE_PATH);
-
-				$keysets = array();
-
-				foreach ($arrUpdated as $class => $details) {
-					$key = sha1($class);
-					$keysets[$key][$class] = $details;
-				}
-
-				foreach ($keysets as $key => $details) {
-					$cache->save($details, $pathKey.'_'.$key);
-				}
-			}
-		}
-
-	}
-
-	public function updateCMSFieldSecondary(FieldList $fields){
-		$this->updateCMSFields($fields);
-	}
-
-	public function updateCMSFields(FieldList $fields){
-        foreach($fields->dataFields() as $field) {
-            if($field instanceof HtmlEditorField) {
-                $attributes = $field->getAttributes();
-
-                $fields->replaceField($field->getName(),
-                    MarkdownEditorField::create($field->getName(), $field->Title())->setRows($attributes['rows']));
+                        Config::inst()->update($className, 'db', $updateDB);
+                    }
+                }
             }
         }
-	}
+    }
+
+    public function updateCMSFieldSecondary(FieldList $fields){
+        $this->updateCMSFields($fields);
+    }
+
+    public function updateCMSFields(FieldList $fields){
+        if(Config::inst()->get('MarkdownExtension', 'replace_html_fields')){
+            foreach($fields->dataFields() as $field) {
+                if($field instanceof HtmlEditorField) {
+                    $attributes = $field->getAttributes();
+
+                    $fields->replaceField($field->getName(),
+                        MarkdownEditorField::create($field->getName(), $field->Title())->setRows($attributes['rows']));
+                }
+            }
+        }
+    }
 
 } 
