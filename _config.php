64dcf449bfb8ac9f8ed9bef543a503e7f953a7e7
Morven Lewis-Everley <morven@i-lateral.com>
2023-06-20 16:30:32 +0100

    Move config setup into yml

--- _config.php
+++ _config.php
@@ -1,13 +1,3 @@
 <?php
 
-use SilverStripe\Core\ClassInfo;
-use SilverStripe\Core\Object;
-use MadeHQ\Markdown\Forms\MarkdownEditorField;
-
 define('MARKDOWN_MODULE_BASE', 'mademedia/silverstripe-markdown');
-
-if (ClassInfo::exists('MadeHQ\Cloudinary\Forms\File')) {
-    MarkdownEditorField::add_extension(
-        'MadeHQ\Markdown\Extensions\MarkdownCloudinaryUpload'
-    );
-}
